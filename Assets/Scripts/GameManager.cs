5579202069d2ad5b8b80d0751325b5583448a134
Newton <65068273+newtonHesienberg@users.noreply.github.com>
2021-04-06 17:24:55 +0530

    Slider Implemented

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,6 +10,12 @@ public class GameManager : MonoBehaviour
     public float gameSpeed = 7.0f;
     public float speedIncreaser = 0.001f;
 
+    [Header("Slider")]
+    public Vector3 targetSliderScale;
+    public Vector3 targetEffectScale;
+    public float sliderSpeed = 2.0f;
+
+    [Header("Tile")]
     public GameObject destroyEffect;
     public Color color;
 
@@ -22,17 +29,47 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("k");
-
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 
             if (hit.collider != null)
             {
-                hit.collider.gameObject.GetComponent<SpriteRenderer>().color = color;
-                Instantiate(destroyEffect, hit.collider.transform.position, Quaternion.identity);
-
+                if (hit.collider.CompareTag("Tiles"))
+                    NormalTileDestroy(hit);
             }
         }
+
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.CompareTag("Slider"))
+                    Sliding(hit);
+            }
+        }
+    }
+
+    private void Sliding(RaycastHit2D hit)
+    {
+        hit.collider.transform.GetChild(0).localScale = Vector3.Lerp(hit.collider.transform.GetChild(0).localScale, targetSliderScale, Time.deltaTime * sliderSpeed);
+        hit.collider.transform.GetChild(3).localScale = Vector3.Lerp(hit.collider.transform.GetChild(3).localScale, targetEffectScale, Time.deltaTime * sliderSpeed);
+
+        if (hit.collider.transform.GetChild(3).localScale.y > 0.85f)
+        {
+            Destroy(hit.collider.transform.GetChild(0).gameObject);
+            Destroy(hit.collider.transform.GetChild(1).gameObject);
+            Destroy(hit.collider.transform.GetChild(2).gameObject);
+            Destroy(hit.collider.transform.GetChild(3).gameObject);
+
+            hit.collider.gameObject.GetComponent<SpriteRenderer>().color = color;
+            Instantiate(destroyEffect, hit.collider.transform.position, Quaternion.identity);
+        }
+    }
+    private void NormalTileDestroy(RaycastHit2D hit)
+    {
+        hit.collider.gameObject.GetComponent<SpriteRenderer>().color = color;
+        Instantiate(destroyEffect, hit.collider.transform.position, Quaternion.identity);
     }
 
     private void FixedUpdate()
